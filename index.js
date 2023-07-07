79cdd9f09bdedb17d099b7834b8e953aa680d162
LysovMaxim <max_maran@icloud.com>
2023-07-07 16:38:17 +0300

    Видалив закоментовані строки та форматував за допомогою prettier

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const contacts = require("./contacts");
-// const yargs = require('yargs');
-// const {hideBin} = require("yargs/helpers")
-const argv = require('yargs').argv;
-
-
+const argv = require("yargs").argv;
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
@@ -17,24 +13,15 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
 
     case "add":
       const newContact = await contacts.addContact({ name, email, phone });
-      return console.log(newContact)
+      return console.log(newContact);
 
     case "remove":
-          const deleteContact = await contacts.removeContact(id)
-          return console.log(deleteContact)
+      const deleteContact = await contacts.removeContact(id);
+      return console.log(deleteContact);
 
     default:
       console.warn("\x1B[31m Unknown action type!");
   }
 };
 
-
-// const arr = hideBin(process.argv)
-// const { argv } = yargs(arr)
-// console.log(argv)
-invokeAction(argv)
-
-// invokeAction({ action: "list" })
-// invokeAction({ action: "get", id: "AeHIrLTr6JkxGE6SN-0Rw" });
-// invokeAction({ action: "add", name: "Maxim Lysov", email: "maxim.ante@vestibul.co.uk", phone: "(111) 914-3792" });
-// invokeAction({ action: "remove", id: "e6ywwRe4jcqxXfCZOj_1e" });
+invokeAction(argv);
